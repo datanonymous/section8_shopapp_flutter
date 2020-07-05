a3882d6e95260f7a03f38ec2b43cd0f0bf0e1341
Alex Ko <ko.e.alexander@gmail.com>
2020-07-05 13:44:11 -0400

    7.5.2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import './screens/edit_product_screen.dart';
+import './screens/user_products_screen.dart';
 import './providers/cart_provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
@@ -51,6 +53,8 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx)=> OrdersScreen(),
+          UserProductsScreen.routeName: (ctx)=> UserProductsScreen(),
+          EditProductScreen.routeName: (ctx)=> EditProductScreen(),
         },
       ),
     );
---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:section8_shopapp_flutter/screens/edit_product_screen.dart';
+import '../widgets/app_drawer.dart';
+import '../providers/products_provider.dart';
+import '../widgets/user_product_item.dart';
+import './edit_product_screen.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user_products_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your products'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                title: productsData.items[i].title,
+                imageUrl: productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

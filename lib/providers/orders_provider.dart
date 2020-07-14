0b8e215facddb039bb29f544adc86de2fcd5a8e7
Alex Ko <ko.e.alexander@gmail.com>
2020-07-14 13:05:01 -0400

    7.14.2020

--- lib/providers/orders_provider.dart
+++ lib/providers/orders_provider.dart
@@ -48,7 +48,7 @@ class OrdersProvider with ChangeNotifier {
               .toList(),
           dateTime: DateTime.parse(value['dateTime'])));
     });
-    _orders = loadedOrders;
+    _orders = loadedOrders.reversed.toList();
     notifyListeners();
   }
 
